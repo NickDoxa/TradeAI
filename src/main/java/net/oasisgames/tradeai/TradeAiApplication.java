1569f018421e1c073f6042a8933f51dcba22f8f7
Nick Doxa <77412044+NickDoxa@users.noreply.github.com>
2024-12-17 00:32:14 -0500

    initial commit

---
+++ src/main/java/net/oasisgames/tradeai/TradeAiApplication.java
@@ -0,0 +1,13 @@
+package net.oasisgames.tradeai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/oasisgames/tradeai/TradeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.oasisgames.tradeai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
